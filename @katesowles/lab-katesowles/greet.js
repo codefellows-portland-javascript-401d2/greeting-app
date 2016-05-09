762f2cf057c2a1ab62c053a32d1c4d4c4d1a68bb
Kate Sowles <katesowles@gmail.com>
2016-05-09 14:53:36 -0700

    it would help if i inluded ALL the files

---
+++ @katesowles/lab-katesowles/greet.js
@@ -0,0 +1,4 @@
+module.export = function greet(name='no one') {
+  console.log(name);
+  return 'hello, ' + name + ', how is your day going?';
+};
--- @katesowles/sandbox/add.js
+++ @katesowles/sandbox/add.js
@@ -1,14 +1,12 @@
-var magic = require('./magic');
-
-magic.foo = 'BAR';
-
-console.log('magic', magic);
+// var magic = require('./magic');
+// magic.foo = 'BAR';
+// console.log('magic', magic);
 
 module.exports = function add(x=0,y=0) {
   return x + y;
 };
 
-console.log( __dirname ); // this si what the "requires" are relative to
+// console.log( __dirname ); // this is what the "requires" are relative to
 
 // function subtract() {}
 
