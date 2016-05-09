aa87a206fefcf7d6378fd198b8ed27ee70359e00
goodwid <goodwid@gmail.com>
2016-05-09 16:22:35 -0700

    Added test for command line feature

---
+++ lab-goodwid/test/index.test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var es = require('child_process').execSync;
+var test1 = es('node index.js David', {encoding: 'UTF-8'});
+var test2 = es('node index.js', {encoding: 'UTF-8'});
+
+
+describe('Testing command line app', () => {
+  it('\'node index.js David\' displays \'Hello David!\'', () => {
+    assert.equal(test1, 'Hello David!\n');
+  });
+
+  it('\'node index.js\' displays \'Hello friend!\'', () => {
+    assert.equal(test2, 'Hello friend!\n');
+  });
+});
