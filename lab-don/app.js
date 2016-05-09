f1cb884fc96abdbe4c3785577a6d07d11a471ba1
DonChatelain <djchatelain@gmail.com>
2016-05-09 16:36:05 -0700

    cleans up code

--- lab-don/app.js
+++ lab-don/app.js
@@ -1,6 +1,7 @@
 var hello = require('./hello');
 var input = process.argv;
 var name = '';
+
 // Handles input multiple names separated by space
 for (var i = 2; i < input.length; i++) {
   name += input[i] + ' ';
--- lab-don/tests/test.js
+++ lab-don/tests/test.js
@@ -2,10 +2,8 @@ var assert = require('assert');
 var hello = require('../hello');
 var execSync = require('child_process').execSync;
 
-
-
 describe('greeting', () => {
-    
+  
   it('greets input string', () => {
     assert.equal(hello.sayHello('Don'), 'Hello, Don'); 
   });
@@ -17,10 +15,12 @@ describe('greeting', () => {
   it('greets input numbers', () => {
     assert.equal(hello.sayHello(328), 'Hello, 328'); 
   });
+  
   it('properly receives command line input', () => {
     assert.equal(execSync('node app.js Dingle').toString(), 
     'Hello, Dingle \n');
   });
+  
   it('receives multiple names (i.e first and last)', () => {
     assert.equal(assert.equal(execSync('node app.js Dingle McFuss').toString(), 
     'Hello, Dingle McFuss \n'));
