d0f54d34213fa1ccea18cf29c13fffae5bac07f4
Kate Sowles <katesowles@gmail.com>
2016-05-09 14:38:10 -0700

    pushing up sandbox changes

---
+++ @katesowles/sandbox/add.js
@@ -0,0 +1,25 @@
+var magic = require('./magic');
+
+magic.foo = 'BAR';
+
+console.log('magic', magic);
+
+module.exports = function add(x=0,y=0) {
+  return x + y;
+};
+
+console.log( __dirname ); // this si what the "requires" are relative to
+
+// function subtract() {}
+
+// exports.add = add;
+// exports.subtract = subtract;
+// is the same as...
+// module.exports.add = add;
+// module.exports.subtract = subtract;
+// and...
+// module.exports = {
+//   add: add,
+//   subtract: subtract
+// };
+// BUT, don't reassign "exports" unless you're qualifying it with "module".
