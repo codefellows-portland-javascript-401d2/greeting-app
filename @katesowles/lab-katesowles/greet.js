622e9ce7b02bd6f9fdb29f2e8f427c69e2503101
Kate Sowles <katesowles@gmail.com>
2016-05-10 14:06:34 -0700

    added gitignore, changelog, gulpfile, package.json files and incorporated the cool-ascii-faces npm package to the greeting

--- @katesowles/lab-katesowles/greet.js
+++ @katesowles/lab-katesowles/greet.js
@@ -1,3 +1,5 @@
-module.exports = function greet(name='no one') {
-  return 'hello ' + name + ', how is your day going?';
+const faces = require("cool-ascii-faces");
+
+module.exports = function greet(name="no one") {
+    return `hello ${name}, how is your day going? ${faces()}`;  // using back ticks so I can utilize the ${} format
 };
--- @katesowles/lab-katesowles/test/greet.test.js
+++ @katesowles/lab-katesowles/test/greet.test.js
@@ -1,11 +1,14 @@
-var assert = require('assert');
-var greet = require('../greet');
+var assert = require("assert");
+var greet = require("../greet");
 
-describe ('greetings', () => {
-  it('greets properly', function() {
-    assert.equal(greet('kate'), 'hello kate, how is your day going?');
-  });
-  it('no name = no one', function() {
-    assert.equal(greet(), 'hello no one, how is your day going?');
-  });
+describe ("greetings", () => {
+    it("greets properly", function() {
+        const greeting = greet("kate");
+        assert.ok( /hello kate, how is your day going?/.test(greeting), `result was "${greeting}"`);
+        // assert.ok(greet("kate"), "hello kate, how is your day going?");
+    });
+    it("no name = no one", function() {
+        // const greeting = greet("");
+        assert.ok(greet(), "hello no one, how is your day going?");
+    });
 });
