a8fea05fffb459184509c081a698365093881319
Allison Davis <a.rae.davis@gmail.com>
2016-05-09 21:15:40 -0700

    add test for command line feature

--- lab-allison/test/greet.test.js
+++ lab-allison/test/greet.test.js
@@ -1,5 +1,6 @@
 var assert = require( 'assert' );
 var greet = require( '../greet');
+var execSync = require('child_process').execSync;
 
 describe('greeting', function(){
   it('returns greeting with name parameter', function(){
@@ -8,5 +9,10 @@ describe('greeting', function(){
 
   it('returns a string', function(){
     assert.equal(typeof greet.hello('allison'), 'string');
+  });
+
+  it('arguments are used correctly', function(){
+    var out = execSync('node ../greet.js Ginsburg');
+    assert.equal(out, 'hello Ginsburg\n');
   });
 });
