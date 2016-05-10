55725188cbf63f50cde1f622526a4d1a22ba0dd7
Allison Davis <a.rae.davis@gmail.com>
2016-05-10 14:17:37 -0700

    add CHANGELOG; add gulpfile; run testing and linting through gulp

---
+++ lab-allison/CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+
+##[unreleased]
+### Added
+### Changed
+### Removed
+
+## [1.1.0] - 2016-05-10
+### Added
+-Add npm/package.json file for dependency management
+-Add gulp for linting and testing automation
+
+### Changed
+-Add tests for blank name
+-Add eslint and new linter rules
+-npm scripts use gulp for testing and linting
+
+### Removed
+-Remove test for typeof output (redundant)
+
+## [1.0.0] - 2016-05-09
+### Added
+-initial release
+-command line utility allows user name input
+-create Mocha tests
---
+++ lab-allison/gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+gulp.task('lint', function(){
+  return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('run-test', function(){
+  return gulp.src('./test/**/*.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watch-js', function(){
+  gulp.watch(['**/*.js', '!node_modules/**'], ['lint', ['test']]);
+});
+
+//TODO set up npm scripts and default task 
