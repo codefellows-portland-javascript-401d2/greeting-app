74ed805fb701c5ec4327960ab5911fa5d3a10ee6
Kate Sowles <katesowles@gmail.com>
2016-05-10 14:13:39 -0700

    updating test for no entry to make sure it displays "no one" instead of an empty string.

--- @katesowles/lab-katesowles/greet.js
+++ @katesowles/lab-katesowles/greet.js
@@ -1,5 +1,5 @@
 const faces = require("cool-ascii-faces");
 
-module.exports = function greet(name="no one") {
+module.exports = function greet(name = "no one") {
     return `hello ${name}, how is your day going? ${faces()}`;  // using back ticks so I can utilize the ${} format
 };
--- @katesowles/lab-katesowles/test/greet.test.js
+++ @katesowles/lab-katesowles/test/greet.test.js
@@ -5,10 +5,9 @@ describe ("greetings", () => {
     it("greets properly", function() {
         const greeting = greet("kate");
         assert.ok( /hello kate, how is your day going?/.test(greeting), `result was "${greeting}"`);
-        // assert.ok(greet("kate"), "hello kate, how is your day going?");
     });
     it("no name = no one", function() {
-        // const greeting = greet("");
-        assert.ok(greet(), "hello no one, how is your day going?");
+        const greeting = greet();
+        assert.ok( /hello no one, how is your day going?/.test(greeting), `result was "${greeting}"`);
     });
 });
