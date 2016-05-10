326433abf275fbdd1f0ada991bf6761e4ba2eb90
DonChatelain <djchatelain@gmail.com>
2016-05-10 13:21:17 -0700

    greeting is now customized to the time of day, also adds gulpfile.js

--- lab-don/hello.js
+++ lab-don/hello.js
@@ -1,5 +1,18 @@
+var generateGreeting = function() {
+  var greeting;
+  var thisHour = new Date().getHours();
+  if (thisHour >= 0 && this < 12) {
+    greeting = 'Good Morning';
+  } else if (thisHour >= 12 && thisHour < 18) {
+    greeting = 'Good Afternoon';
+  } else {
+    greeting = 'Good Evening';
+  }
+  return greeting;
+};
+
 var sayHello = function(name = 'Stranger') {
-  return 'Hello, ' + name;  
+  return generateGreeting() + ' ' + name;  
 };
 
 module.exports.sayHello = sayHello;
