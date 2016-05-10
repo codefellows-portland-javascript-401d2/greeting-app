4c0177fb37a382ea29d42ed77a194d09ce59b82e
goodwid <goodwid@gmail.com>
2016-05-10 13:30:56 -0700

    Test for cli parameter added

---
+++ @goodwid/CHANGELOG.md
@@ -0,0 +1,27 @@
+## changelog for greeting-app.
+
+### 1.0.0 (2016-05-06)
+
+#### changes:
+  - gulp installed
+  - mocha installed
+  - gulpfile created
+  - directory name changed
+  - tests automated
+
+
+
+
+<!---
+
+Template for new additions.
+
+### 1.0.0 (2016-05-06)
+
+#### changes:
+  -
+#### features:
+  -  
+#### bugfixes:
+
+-->
--- @goodwid/test/index.test.js
+++ @goodwid/test/index.test.js
@@ -1,15 +1,24 @@
 var assert = require('assert');
 var es = require('child_process').execSync;
-var test1 = es('node index.js David', {encoding: 'UTF-8'});
-var test2 = es('node index.js', {encoding: 'UTF-8'});
+
+
 
 
 describe('Testing command line feature, should greet the names supplied as arguments.', () => {
+  var test1 = es('node index.js David', {encoding: 'UTF-8'});
+  var test2 = es('node index.js', {encoding: 'UTF-8'});
   it('\'node index.js David\' displays \'Hello David!\'', () => {
     assert.equal(test1, 'Hello David!\n');
   });
 
   it('\'node index.js\' displays \'Hello friend!\'', () => {
     assert.equal(test2, 'Hello friend!\n');
+  });
+});
+
+describe('Testing command line parameters.', () => {
+  var test3 = es('node -t index.js David', {encoding: 'UTF-8'});
+  it('\'node index.js -t David\' displays \'Good Evening David!\'', () => {
+    assert.equal(test3, 'Good Evening David!\n');
   });
 });
