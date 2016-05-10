b52167f654d21e5b6ff7ed1a46ef751e5ee36956
Yvonne Hayes <queyvonne@gmail.com>
2016-05-10 13:43:28 -0700

    added more options to greeting function

--- @yvonne/greet.js
+++ @yvonne/greet.js
@@ -1,3 +1,10 @@
 module.exports = function greet (name = 'stranger') {
-  return 'Hello, ' + name;
+  var now = new Date();
+  if (now.getHours() < 12) {
+    return 'Good Morning, ' + name;
+  } else if ((now.getHours() <= 13) && (now.getHours() < 18)) {
+    return 'Good Afternoon, ' + name;
+  } else {
+    return 'Good Evening, ' + name;
+  }
 };
