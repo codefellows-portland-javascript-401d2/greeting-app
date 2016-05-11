30ac17c1d785a04c8b25b6b638cbe2d676b374f4
Kate Sowles <katesowles@gmail.com>
2016-05-11 10:25:00 -0700

    added options, another library

--- @katesowles/greet.js
+++ @katesowles/greet.js
@@ -1,7 +1,8 @@
-const faces = require("cool-ascii-faces");
+const emoji = require("random-emoji");
 
 module.exports = function greet(name = "no one") {
-    var today = new Date().getDay();
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return `hello ${name}, how is your ${days[today]} going? ${faces()}`;  // using back ticks so I can utilize the ${} format
+    var today = new Date().getDay();
+    var emojis = emoji.random({count:1}).map(e => e.character).join("  ");
+    return `hello ${name}, how is your ${days[today]} going? ${emojis}`;  // using back ticks so I can utilize the ${} format
 };
