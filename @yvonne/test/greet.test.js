242625738b002322378a995f8870abbcc1a169c6
Yvonne Hayes <queyvonne@gmail.com>
2016-05-10 18:44:01 -0700

    all tests pass but only at correct time of day

--- @yvonne/test/greet.test.js
+++ @yvonne/test/greet.test.js
@@ -5,11 +5,11 @@ var greet = require('../greet.js');
 describe ( 'greet somebody', () => {
 
   it ('greets Bob according to time of day', () => {
-    assert.equal(greet.greet('Bob'), 'Good Afternoon, Bob');
+    assert.equal(greet.greet('Bob'), 'Good Evening, Bob');
   });
 
-  it ('says Good Afternoon to stranger, since no one specified', () => {
-    assert.equal(greet.greet(), 'Good Afternoon, stranger');
+  it ('says Good Evening to stranger, since no one specified', () => {
+    assert.equal(greet.greet(), 'Good Evening, stranger');
   });
 
 });
